67ae01c7b3287277c8827fb8dd3cfcd1ce1d0bfe
Soroush Khanlou <soroushkhanlou@gmail.com>
2022-10-03 18:48:41 -0400

    set up for the preso

--- Sudoku Live.playground/Contents.swift
+++ Sudoku Live.playground/Contents.swift
@@ -23,27 +23,42 @@ do {
     print(solver.board)
 }
 
-do { //hard
-    let boardString = """
-        4.....8.5
-        .3.......
-        ...7.....
-        .2.....6.
-        ....8.4..
-        ....1....
-        ...6.3.7.
-        5..2.....
-        1.4......
-        """
-    let board = Board(boardString: boardString)
-    let solver = Solver(board: board)
 
-    let date = Date()
-    print(board)
-    solver.isSolved
-    try solver.bruteForce()
-    solver.isSolved
-    print(Date().timeIntervalSince(date))
 
-    print(solver.board)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//do { //hard
+//    let boardString = """
+//        4.....8.5
+//        .3.......
+//        ...7.....
+//        .2.....6.
+//        ....8.4..
+//        ....1....
+//        ...6.3.7.
+//        5..2.....
+//        1.4......
+//        """
+//    let board = Board(boardString: boardString)
+//    let solver = Solver(board: board)
+//
+//    let date = Date()
+//    print(board)
+//    solver.isSolved
+//    try solver.solve()
+//    solver.isSolved
+//    print(Date().timeIntervalSince(date))
+//
+//    print(solver.board)
+//}
