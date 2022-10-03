4bbac387ddef8d1033506851a5ab427b647c9c66
Soroush Khanlou <soroushkhanlou@gmail.com>
2022-10-03 13:11:12 -0400

    rearrange stuff for modern swift

--- Sudoku Live.playground/Contents.swift
+++ Sudoku Live.playground/Contents.swift
@@ -2,7 +2,17 @@ import Foundation
 
 
 do {
-    let boardString = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."
+    let boardString = """
+        ..3.2.6..
+        9..3.5..1
+        ..18.64..
+        ..81.29..
+        7.......8
+        ..67.82..
+        ..26.95..
+        8..2.3..9
+        ..5.1.3..
+        """
     let board = Board(boardString: boardString)
     let solver = Solver(board: board)
     
