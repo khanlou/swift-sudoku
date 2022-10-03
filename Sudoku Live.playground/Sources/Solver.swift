c1c1a1e19751b56d699905dc579c2b714cb1eb48
Soroush Khanlou <soroushkhanlou@gmail.com>
2022-10-03 17:37:49 -0400

    add solution

--- Sudoku Live.playground/Sources/Solver.swift
+++ Sudoku Live.playground/Sources/Solver.swift
@@ -12,19 +12,63 @@ public class Solver {
     }
     
     public func solve() {
-        
+        while !isSolved {
+            var foundSomethingThisIteration = false
+            for (offset, cell) in board.cells.enumerated() {
+                if cell.isSettled {
+                    continue
+                }
+
+                let possibleValues = (1...9).filter({ value in
+                    return board.canUpdate(index: offset, toValue: value)
+                })
+
+                if possibleValues.isEmpty {
+                    return // something has gone really wrong
+                }
+
+                if possibleValues.count == 1 {
+                    foundSomethingThisIteration = true
+                }
+
+                try? board.update(index: offset, values: possibleValues)
+            }
+
+            if !foundSomethingThisIteration {
+                return
+            }
+        }
     }
-    
+
+    public func bruteForce() {
+        self.solve()
+
+        if isSolved {
+            return
+        }
+
+        let (offset, cell) = board.cells
+            .enumerated()
+            .sorted(by: { $0.1.values.count < $1.1.values.count })
+            .first(where: { !$0.1.isSettled })!
+
+        for value in cell.values {
+            var copy = board
+            do {
+                try copy.update(index: offset, values: [value])
+            } catch {
+                if error is ConsistencyError {
+                    continue
+                }
+            }
+
+            let solver = Solver(board: copy)
+            solver.bruteForce()
+            if solver.isSolved {
+                self.board = solver.board
+                return
+            }
+        }
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
